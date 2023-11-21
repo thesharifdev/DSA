f765e3dbd28c859268133a5740b2e0de01829c72
thesharifdev <sharif.golang@gmail.com>
2023-11-21 21:19:34 +0600

    learned: O(n)

---
+++ 1-Big O Notation/p3.js
@@ -0,0 +1,12 @@
+function addUpTo(n){
+    let total = 0;
+    for(let i = 1; i <= n; i++){
+        total += i;
+    }
+    return total;
+}
+
+/**
+ * Here time complexity is O(n). Because the function operation is depend on the input. As is input, so is operation.
+ */
+
